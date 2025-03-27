81a1ae564342658a7c61646ff0a950e0570faaea
nikhilljpskj <nikhiljp.skj@gmail.com>
2025-03-28 01:14:43 +0530

    Falken-Updated, Home Talents Contact Email and Newsletter About Page deleted pnpm

--- components/Talents/TalentsSectionTwo.tsx
+++ components/Talents/TalentsSectionTwo.tsx
@@ -5,25 +5,7 @@ const TalentsSectionTwo = () => {
     <section className="py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4 lg:w-1/2">
-            <div
-              className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
-              data-wow-delay=".15s"
-            >
-              <Image
-                src="/images/talents/about-image-2.svg"
-                alt="FalkenRidge Talents Bootcamp"
-                fill
-                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
-              />
-              <Image
-                src="/images/talents/about-image-2-dark.svg"
-                alt="FalkenRidge Talents Bootcamp"
-                fill
-                className="drop-shadow-three hidden dark:block dark:drop-shadow-none"
-              />
-            </div>
-          </div>
+          
           <div className="w-full px-4 lg:w-1/2">
             <div className="wow fadeInUp max-w-[470px]" data-wow-delay=".2s">
               <div className="mb-9">
@@ -50,6 +32,25 @@ const TalentsSectionTwo = () => {
                   We don’t just train, we transform. Get guidance from professionals, build a strong portfolio, and land your dream job in tech.
                 </p>
               </div>
+            </div>
+          </div>
+          <div className="w-full px-4 lg:w-1/2">
+            <div
+              className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
+              data-wow-delay=".15s"
+            >
+              <Image
+                src="/images/talents/about-image-2.svg"
+                alt="FalkenRidge Talents Bootcamp"
+                fill
+                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
+              />
+              <Image
+                src="/images/talents/about-image-2-dark.svg"
+                alt="FalkenRidge Talents Bootcamp"
+                fill
+                className="drop-shadow-three hidden dark:block dark:drop-shadow-none"
+              />
             </div>
           </div>
         </div>
