b82d5f82e1156efc503e30ac5108b509b93be0fe
naayifmuhammad <muhdnaayif@gmail.com>
2025-04-06 19:21:28 +0530

    Created saas section and careers section

---
+++ app/careers/page.tsx
@@ -0,0 +1,38 @@
+const CareersPage = () => {
+    return (
+      <div className="container mx-auto px-6 py-12 min-h-screen mt-14">
+        <div className="max-w-5xl mx-auto p-8 rounded-lg">
+          <h1 className="text-3xl font-bold mb-4 ">Careers at FalkenRidge</h1>          
+          <p className="mb-4 leading-relaxed">
+            At FalkenRidge, we’re on a mission to revolutionize digital solutions through innovative SaaS products and modern web development. We infuse startup energy into everything we do—challenging the status quo and building technologies that empower businesses.
+          </p>
+          
+          <p className="mb-4 leading-relaxed ">
+            Our core technology stack includes Next.js, React, Tailwind CSS, and Node.js. We leverage these tools to create scalable, robust, and visually stunning applications.
+          </p>
+          
+          <p className="mb-4 leading-relaxed ">
+            While we currently don’t have any open positions, we’re always excited to connect with creative problem-solvers, innovative thinkers, and passionate developers who thrive in a fast-paced startup environment. If you’re eager to contribute, learn, and grow with us, please don’t hesitate to reach out.
+          </p>
+          
+          <p className="mb-4 leading-relaxed ">
+            Send your resume and a brief introduction to&nbsp;
+            <a
+              href="mailto:careers@falkenridge.com"
+              className="text-blue-600 hover:underline"
+            >
+              careers@falkenridge.com
+            </a>
+            . Stay tuned for future opportunities as we continue to expand our team and innovate.
+          </p>
+          
+          <p className="mb-4 leading-relaxed ">
+            Join us in building the future of digital solutions!
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default CareersPage;
+  
--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -55,13 +55,13 @@ const AboutSectionOne = () => {
                 data-wow-delay=".2s"
               >
                 <Image
-                  src="/images/about/about-image.svg"
+                  src="/images/about/about-section-img-light.svg"
                   alt="FalkenRidge - Digital Solutions"
                   fill
                   className="drop-shadow-three mx-auto max-w-full dark:hidden dark:drop-shadow-none lg:mr-0"
                 />
                 <Image
-                  src="/images/about/about-image-dark.svg"
+                  src="/images/about/about-section-img-dark.svg"
                   alt="FalkenRidge - Digital Solutions"
                   fill
                   className="drop-shadow-three mx-auto hidden max-w-full dark:block dark:drop-shadow-none lg:mr-0"
