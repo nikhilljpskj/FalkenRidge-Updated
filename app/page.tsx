d565ad4e84fa8b59d56407c20be85ab017e46109
naayifmuhammad <muhdnaayif@gmail.com>
2025-04-06 19:59:50 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Hero from "@/components/Hero";
 //import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
-import SaaS from "@/components/saas";
+import SaaS from "@/components/saas/saas";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
--- components/saas/saas.tsx
+++ components/saas/saas.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import SectionTitle from "../Common/SectionTitle";
-import SaasIntro from "@/components/saasIntro";
+import SaasIntro from "@/components/saasIntro/saadIntro";
 
 
 const checkIcon = (
